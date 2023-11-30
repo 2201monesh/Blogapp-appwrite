946409856dd4ff2886c73c21a8502135dad0caaf
Monesh Goyal <goyalmonesh2201@gmail.com>
2023-11-30 17:20:49 +0530

    getPost getPosts in config.js

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -57,15 +57,45 @@ export class Service{
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
                 slug,
-                {
-
-                }
             );
             return true;
         } catch (error) {
             console.log("Appwrite Service :: deletePost :: error", error);
             return false
         }
+    }
+
+    async getPost(slug){
+        try {
+            return await this.databases.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+            );
+            return true;
+        } catch (error) {
+            console.log("Appwrite Service :: getPost :: error", error);
+            return false
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status", "active")]){
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries
+            );
+        } catch (error) {
+            console.log("Appwrite Service :: getPosts :: error", error);
+            return false
+        }
+    }
+
+    // File upload service
+
+    async uploadFile(file){
+        
     }
 
 }
