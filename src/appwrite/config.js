401fead34d555efda15dd96891b7d9d9a1430e61
Monesh Goyal <goyalmonesh2201@gmail.com>
2023-11-30 17:39:43 +0530

    basic appwrite configurations done

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -95,7 +95,36 @@ export class Service{
     // File upload service
 
     async uploadFile(file){
-        
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            );
+        } catch (error) {
+            console.log("Appwrite Service :: uploadFile :: error", error);
+            return false
+        }
+    }
+
+    async deleteFile(fileId){
+        try {
+            await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            );
+            return true
+        } catch (error) {
+            console.log("Appwrite Service :: deleteFile :: error", error);
+            return false
+        }
+    }
+
+    getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        );
     }
 
 }
